da860b5823690ff9e2c4ef66bd90a7552eb052f6
Alvin Savoy <alvin.savoy@gmail.com>
2013-04-23 12:14:35 +1000

    Removed unnecessary init code that was breaking when installing with Django 1.5

--- activeusers/__init__.py
+++ activeusers/__init__.py
@@ -5,22 +5,3 @@ def get_version():
     "Returns the version as a human-format string."
     return '.'.join([str(i) for i in VERSION])
 
-# initialize the URL prefixes that we shouldn't track
-try:
-    from django.conf import settings
-    prefixes = getattr(settings, 'ACTIVEUSERS_IGNORE_PREFIXES', [])
-except ImportError:
-    pass
-else:
-    if '!!initialized!!' not in prefixes:
-        from django.core.urlresolvers import reverse, NoReverseMatch
-
-        keys = ('MEDIA_URL', 'STATIC_URL')
-        for key in keys:
-            if hasattr(settings, key) and getattr(settings, key) != '/':
-                prefixes.append(getattr(settings, key))
-        
-        prefixes.append('!!initialized!!')
-
-        settings.ACTIVEUSERS_IGNORE_PREFIXES = prefixes
-
